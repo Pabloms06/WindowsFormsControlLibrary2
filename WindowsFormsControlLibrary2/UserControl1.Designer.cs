d0f5c203191d4a57c0774e77ee933faee39332d8
Carlos <carlostena@centroafuera.es>
2021-02-10 12:33:42 +0100

    Agregar archivos de proyecto.

---
+++ WindowsFormsControlLibrary2/UserControl1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsControlLibrary2
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(30, 108);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "¿Permitir click?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(30, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Continuar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(285, 316);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+
+        public System.Windows.Forms.CheckBox getCheckBox()
+        {
+            return checkBox1;
+        }
+
+        public System.Windows.Forms.Button getButton()
+        {
+            return button1;
+        }
+
+        [System.ComponentModel.Category("Comportamiento")]
+        public string PropiedadDeCarlos { set; get; }
+    }
+}
---
+++ WindowsFormsControlLibrary2/UserControl1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary2
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (PropiedadDeCarlos != null && PropiedadDeCarlos != "")
+            {
+                MessageBox.Show(PropiedadDeCarlos, "Bien!", MessageBoxButtons.OK);
+
+            }
+            else
+            {
+                MessageBox.Show("Click realizado", "Bien!", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = checkBox1.Checked;
+        }
+    }
+}
